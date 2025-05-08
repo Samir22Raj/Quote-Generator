3dd1239062464a0f5c8b12fe40fc54c456b7aa13
Samir <srajkarnikar9@gmail.com>
2025-05-08 12:07:27 +0545

    added next and previous buttons and functions for them

--- script.js
+++ script.js
@@ -4,16 +4,31 @@ document.addEventListener("DOMContentLoaded", async () => {
 		const response = await fetch("quotes.json");
 		if (!response.ok) throw new Error("Failed to load JSON");
 		const data = await response.json();
-		console.log(data);
-		let randombutton = document.getElementById("btnrandom");
+		let randombutton = document.getElementById("randombtn");
+		let prevbutton = document.getElementById("prevbtn");
+		let nextbutton = document.getElementById("nxtbtn");
 		let quoteText = document.getElementById("quote");
+
+		let quoteNumber = 0;
+		quoteText.innerHTML = data[quoteNumber].quote;
+
+		//Event lister for previous qoute
+		prevbutton.addEventListener("click", () => {
+			quoteNumber == 0 ? quoteNumber = data.length - 1: quoteNumber--;
+			quoteText.innerHTML = data[quoteNumber].quote;
+		});
+		
+		//Event lister for next qoute
+		nextbutton.addEventListener("click", () => {
+			quoteNumber == data.length - 1? quoteNumber = 0: quoteNumber++;
+			quoteText.innerHTML = data[quoteNumber].quote;
+		});
 
     //Event lister for generating a random quote on btnrandom
 		randombutton.addEventListener("click", () => {
 			let random = Math.floor(Math.random() * 15);
 			console.log(random);
 			quoteText.innerHTML = data[random].quote;
-			quoteText.classList.remove("hide");
 		});
 
 	} catch (error) {
